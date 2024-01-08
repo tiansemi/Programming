f890a44c1a04b295af65cd279456d15f6ba774e7
TianSemi Corp <tiansemi@outlook.com>
2024-01-08 10:54:13 +0000

    Create testFenetre.java

---
+++ JAVA/Exercices_University/Activity006/testFenetre.java
@@ -0,0 +1,15 @@
+package ci.inphb.Larima.*;
+
+public class testFenetre {
+    public static void main(String[] args) {
+        Point p = new Point(5, 10);
+        Fenetre f = new Fenetre(p, 20, 30);
+        f.afficher();
+    }
+}
+
+// Execution code :
+// javac -d ../packs Point.java
+// javac -d ../packs -cp ../packs Fenetre.java
+// javac -cp ../packs  testFenetre.java
+// java -cp "../packs:." testFenetre
