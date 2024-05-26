d0ebdd76da8c6322cd69da86197d4bd1483bfcc8
TianSemi Corp <tiansemi@outlook.com>
2024-05-26 11:27:51 +0000

    Download multiple files from base url with wget

---
+++ Shell-Linux/autodownload.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+: "
+@author : tiansemi@outlook.com
+steps : 
+
+$ mkdir webfonts && cd webfonts
+$ chmod +x download.sh
+$ ./autodownload.sh
+
+NB : Ce script lit le fichier requirements.txt ligne par ligne, et chaque ligne est considérée comme le nom d’un fichier à télécharger. Assurez-vous que le fichier requirements.txt est dans le même répertoire que le script, ou modifiez le chemin du fichier en conséquence
+"
+
+# URL de base
+base_url="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/webfonts/"
+
+# Lire le fichier requirements.txt
+while IFS= read -r file
+do
+  # Construction de l'URL du fichier
+  file_url="${base_url}${file}"
+  
+  # Téléchargement du fichier
+  wget $file_url
+done < requirements.txt
