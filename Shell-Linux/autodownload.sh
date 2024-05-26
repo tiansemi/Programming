404767cf7fb5a6049b4ab4bd4c6446a5423d56d6
TianSemi Corp <tiansemi@outlook.com>
2024-05-26 11:31:15 +0000

    Update autodownload.sh

--- Shell-Linux/autodownload.sh
+++ Shell-Linux/autodownload.sh
@@ -9,6 +9,22 @@ $ chmod +x download.sh
 $ ./autodownload.sh
 
 NB : Ce script lit le fichier requirements.txt ligne par ligne, et chaque ligne est considérée comme le nom d’un fichier à télécharger. Assurez-vous que le fichier requirements.txt est dans le même répertoire que le script, ou modifiez le chemin du fichier en conséquence
+Exemple de contenu du requirements.txt  :
+fa-solid-900.eot
+fa-solid-900.woff2
+fa-solid-900.woff
+fa-solid-900.ttf
+fa-solid-900.svg
+fa-regular-400.eot
+fa-regular-400.woff2
+fa-regular-400.woff
+fa-regular-400.ttf
+fa-regular-400.svg
+fa-brands-400.eot
+fa-brands-400.woff2
+fa-brands-400.woff
+fa-brands-400.ttf
+fa-brands-400.svg 
 "
 
 # URL de base
