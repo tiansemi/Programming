bde7f13109cf78a1f8a633c4cd003644ba51d24b
TianSemi Corp. <tiansemi@outlook.com>
2025-09-23 22:27:17 +0000

    Remove LED pin setup from bp.ino
    Removed LED pin definitions and related setup code.

--- C-Arduino/esp32/bp.ino
+++ C-Arduino/esp32/bp.ino
@@ -1,19 +1,9 @@
-const int ledRedPin = 14;
-const int ledGreenPin = 23;
-const int ledBluePin = 26;
 const int bpPin = 35;
-unsigned int toggle = 0;
 
 void setup() {
   Serial.begin(115200);
-  pinMode(ledRedPin, OUTPUT);
-  pinMode(ledGreenPin, OUTPUT);
-  pinMode(ledBluePin, OUTPUT);
   pinMode(bpPin, INPUT_PULLDOWN);
-
   Serial.println("\n✅ prêt !");
-  digitalWrite(ledRedPin, !toggle);
-  digitalWrite(ledGreenPin, toggle);
 }
 
 void loop() {
