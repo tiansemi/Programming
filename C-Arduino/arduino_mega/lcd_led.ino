5391a2ddface52d0ac16d0d364c7b4f8c8fcea5b
TianSemi Corp. <tiansemi@outlook.com>
2026-01-20 13:55:15 +0000

    Optimize delay times in lcd_led.ino
    Reduced delays in system initialization and display update.

--- C-Arduino/arduino_mega/lcd_led.ino
+++ C-Arduino/arduino_mega/lcd_led.ino
@@ -3,7 +3,7 @@
  * My I2C is : 0x27, please yours with code from
  * https://www.geeksforgeeks.org/electronics-engineering/how-to-interface-i2c-lcd-display-with-arduino/
  */
- 
+#include <Wire.h>  
 #include <LiquidCrystal_I2C.h>
 
 LiquidCrystal_I2C lcd(0x27, 20, 4); // please put correct I2C address
@@ -14,29 +14,29 @@ void setup()
     // 1. Start Serial communication at 9600 baud
     Serial.begin(9600);
     Serial.println("--- System Initializing ---");
-    delay(500);
+    delay(100);
     // Initialize the LCD
     lcd.init();
     lcd.backlight();
     Serial.println("LCD Initialized.");
-    delay(500);
+    delay(100);
     pinMode(ledR, OUTPUT);
 }
 
 void loop()
 {
     Serial.println("Updating Display...");
-    delay(500);    
-    lcd.clear(); 
+    delay(100);    
     lcd.setCursor(0, 0);
     lcd.print("Hello"); 
     lcd.setCursor(0, 1);
     lcd.print("TianSemi");
-    delay(500);
+    delay(100);
     digitalWrite(ledR, HIGH);
     Serial.println("LED Pin 22 set to HIGH.");
-    delay(1000); // Increased delay to make serial output readable
+    delay(10000); // Increased delay to make serial output readable
     digitalWrite(ledR, LOW);
     Serial.println("LED turned OFF.");
-    delay(1000);
+    lcd.clear();
+    delay(500);
 }
